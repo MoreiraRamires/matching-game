34e17305d51e608d8c4ab89c5890f93c3902ec63
Rafael Ramires <rafael-ramires@MacBook-Pro-de-Rafael.local>
2021-09-27 17:32:19 -0300

    ScoreBoard

--- main.js
+++ main.js
@@ -2,16 +2,15 @@ import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
 
-import PlayerName from "./src/components/PlayerName";
 import BoardGame from "./src/objects/BoardGame";
+import ScoreBoard from "./src/objects/ScoreBoard";
 
 const $root = document.querySelector("#root");
 
 $root.insertAdjacentHTML(
   "beforeend",
   `
-    ${PlayerName("Rafael")}
-    ${PlayerName("Isa")}
+    ${ScoreBoard()}
     ${BoardGame(6)}
   `
 );
--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 function CardGame (){
 
 
-  return`
+  return /*html*/`
   <article class="card-game">
     <img class="front-face" src="images/alura-pixel.png" alt="Logo da Alura">
     <img class="back-face" src="img/js-badge.svg" alt="Verso da Carta">
